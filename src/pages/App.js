60a7cfbe42db1a9285ee32cd936420b9057f6909
Gabriel Pontes <gabriel.pontes@suporterenova.onmicrosoft.com>
2025-01-09 23:18:07 -0300

    responsive adjustments

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,27 +3,26 @@ import './App.css';
 import leviosaGif from '../assets/imgs/its-leviosa.gif';
 import historyBook from '../assets/imgs/history-book.png';
 import githubLogo from '../assets/imgs/github-logo.png';
-import Tipography from '../components/font'; 
+import Tipography from '../components/tipography'; 
 
 function App() {
   return (
     <div className="App">
         <div className='Container'>
           <div className='Container-Header'>
-            <Tipography weight='680' size='3.8vh'>
+            <Tipography weight='600' size='5.5vh'>
               VIVA A MAGIA DAS
-              <Tipography weight='800' size='3.8vh'> CARTAS PERDIDAS!</Tipography>
+              <Tipography weight='800' size='5.5vh'> CARTAS PERDIDAS!</Tipography>
             </Tipography>
-            <Tipography weight='900' size='3.5vh'>THE LOST LETTERS</Tipography>
+            <Tipography weight='900' size='5vh'>THE LOST LETTERS</Tipography>
             <img className='img-book' src={historyBook} height={80} width={80} />
 
-            <Tipography weight='light' size='3vh'>Uma Dose de Magia na Sua Caixa de Entrada!</Tipography>
+            <Tipography weight='lighter' size='4vh'>Uma Dose de Magia na Sua Caixa de Entrada!</Tipography>
           </div>
           <img className='leviosa-gif' src={leviosaGif}/>
 
-          <Tipography weight='normal' size='2.3vh'>
+          <Tipography weight='normal' size='2.5vh'>
           Deixe seu e-mail abaixo e receba as Cartas Perdidas diretamente na sua caixa de entrada.
-          <br/>
           Prepare-se para explorar segredos, curiosidades e encantos do universo mágico!
           </Tipography>       
 
@@ -31,16 +30,16 @@ function App() {
             <input placeholder='Email'></input><button>LEVIOSAR!</button>
           </div>
 
-          <Tipography weight='normal' size='2.4vh' fontStyle='italic'>
+          <Tipography weight='normal' size='2vh' fontStyle='italic'>
             <b>The Lost Letters</b> é uma <span className='underline'>newsletter</span> independente criada de um fã para fãs do universo mágico.  
             Não temos qualquer afiliação oficial com J.K. Rowling, Warner Bros., ou quaisquer       
             outras entidades detentoras dos direitos autorais e de marca registrada de Harry Potter.
             Todo o conteúdo é feito com respeito e admiração pela obra original, sem intenção de infringir direitos.
           </Tipography>     
           <a href='https://github.com/GabrielPontess/lost-letters-app'>
-            <img src={githubLogo} height={40} />
+            <img src={githubLogo} height={30} />
           </a>
-          <Tipography weight='normal' size='2.5vh' >
+          <Tipography weight='normal' size='3vh' >
             Enfeitiçado pelo bruxo: <br/>       
             Gabriel Pontes<br/> 
           </Tipography> 
